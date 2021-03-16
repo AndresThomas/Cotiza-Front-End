1f536dd40ce83504af39fe6610f62684f2974985
andres alejandro garcia thomas <173218@ids.upchiapas.edu.mx>
2021-03-16 11:30:56 -0600

    add delete and cost function

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,9 +14,10 @@ import { RequestServiceService } from '../request-service.service';
 })
 export class DashboardComponent implements OnInit {
 
-  results: any ;
-  data:any;
-  products: any[]=[];
+  results: any;
+  data: any;
+  products: any[] = [];
+  totalPagar: number | undefined;
   name: string = localStorage.getItem("name") || "";
 
   constructor(
@@ -26,52 +27,75 @@ export class DashboardComponent implements OnInit {
     private router: Router,
   ) { }
 
-  validation(){
-    if(!this.cookieService.check('cookieLogin')){
-      this.router.navigate(["/"]);   
+  validation() {
+    if (!this.cookieService.check('cookieLogin')) {
+      this.router.navigate(["/"]);
     }
   }
 
-  deleteProduct(e:any,id:number){
+  deleteProduct(e: any, key: number) {
+    var productAux:any ;
+    var del:any;
+    this.products.forEach(element => {
+    
+      if(element.id == key){
+        console.log(element.id,key);
+        console.log(this.products);
+        del = this.products.indexOf(element);
+        productAux = element;
+        this.products.splice(del,1);
+      }
+    });
+    console.log(productAux,'aux, del', del );
     
   }
 
-  extractData(){
+  clean(){
+    this.products = [];
+  }
+
+  total(e: any, costo: number) {
+    var x = (<HTMLInputElement>document.getElementById('cantidadP')).value;
+    var y: number = +x;
+    this.totalPagar = costo * y;
+  }
+
+  extractData() {
     this.request.getProducts().subscribe(
       (results) => {
         this.results = results;
         //console.log(this.results);
       },
-      (error)=>{
+      (error) => {
         console.log(error)
       }
     )
   }
 
-  
+
   openDialog() {
-    this.extractData();    
+    this.extractData();
     const dialogRef = this.matDialog.open(MyDialogComponent, {
       width: '70%',
       height: '70%',
       data: this.results
     });
-    
+
     dialogRef.afterClosed().subscribe(result => {
       console.log(result);
       this.data = result;
       for (let i = 0; i < this.data.length; i++) {
-        
-          this.request.getProduct(this.data[i]).subscribe(
-            (product)=>{
-              this.products.push(product);
-            },(error)=>{
-              console.log(error);
-            }
-            );
-          
+
+        this.request.getProduct(this.data[i]).subscribe(
+          (product) => {
+            this.products.push(product);
+          }, (error) => {
+            console.log(error);
+          }
+        );
+
       }
-      console.log(this.products,'-> products');
+      console.log(this.products, '-> products');
     })
   }
   ngOnInit(): void {
